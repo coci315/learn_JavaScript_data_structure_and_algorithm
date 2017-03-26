251644e809417c1265671caee96d610533125216
coci315 <coci315@gmail.com>
2017-03-26 21:34:14 +0800

    add chapter08

---
+++ chapter08/binarySearchTree.js
@@ -0,0 +1,161 @@
+function BinarySearchTree() {
+  var Node = function (key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  };
+  var root = null;
+
+  // 辅助函数
+  var insertNode = function (node, newNode) {
+    if (newNode.key < node.key) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        insertNode(node.right, newNode);
+      }
+    }
+  };
+
+  var inOrderTraverseNode = function (node, callback) {
+    if (node !== null) {
+      inOrderTraverseNode(node.left, callback);
+      callback(node.key);
+      inOrderTraverseNode(node.right, callback);
+    }
+  };
+
+  var preOrderTraverseNode = function (node, callback) {
+    if (node !== null) {
+      callback(node.key);
+      preOrderTraverseNode(node.left, callback);
+      preOrderTraverseNode(node.right, callback);
+    }
+  };
+
+  var postOrderTraverseNode = function (node, callback) {
+    if (node !== null) {
+      postOrderTraverseNode(node.left, callback);
+      postOrderTraverseNode(node.right, callback);
+      callback(node.key);
+    }
+  };
+
+  var minNode = function (node) {
+    if (node) {
+      while (node && node.left !== null) {
+        node = node.left;
+      }
+      return node.key;
+    }
+    return null;
+  };
+
+  var findMinNode = function (node) {
+    if (node) {
+      while (node && node.left !== null) {
+        node = node.left;
+      }
+      return node;
+    }
+    return null;
+  };
+
+  var maxNode = function (node) {
+    if (node) {
+      while (node && node.right !== null) {
+        node = node.right;
+      }
+      return node.key;
+    }
+    return null;
+  };
+
+  var searchNode = function (node, key) {
+    if (node === null) {
+      return false;
+    }
+    if (key < node.key) {
+      return searchNode(node.left, key);
+    } else if (key > node.key) {
+      return searchNode(node.right, key);
+    } else {
+      return true;
+    }
+  };
+
+  var removeNode = function (node, key) {
+    if (node === null) {
+      return null;
+    }
+    if (key < node.key) {
+      node.left = removeNode(node.left, key);
+      return node;
+    } else if (key > node.key) {
+      node.right = removeNode(node.right, key);
+      return node;
+    } else { //键等于node.key
+      //第一种情况——一个叶节点
+      if (node.left === null && node.right === null) {
+        node = null;
+        return node;
+      }
+      //第二种情况——一个只有一个子节点的节点
+      if (node.left === null) {
+        node = node.right;
+        return node;
+      } else if (node.right === null) {
+        node = node.left;
+        return node;
+      }
+      //第三种情况——一个有两个子节点的节点
+      var aux = findMinNode(node.right);
+      node.key = aux.key;
+      node.right = removeNode(node.right, aux.key);
+      return node;
+    }
+  };
+
+  this.insert = function (key) {
+    var newNode = new Node(key);
+    if (root === null) {
+      root = newNode;
+    } else {
+      insertNode(root, newNode);
+    }
+  };
+
+  this.inOrderTraverse = function (callback) {
+    inOrderTraverseNode(root, callback);
+  };
+
+  this.preOrderTraverse = function (callback) {
+    preOrderTraverseNode(root, callback);
+  };
+
+  this.postOrderTraverse = function (callback) {
+    postOrderTraverseNode(root, callback);
+  };
+
+  this.min = function () {
+    return minNode(root);
+  };
+
+  this.max = function () {
+    return maxNode(root);
+  };
+
+  this.search = function (key) {
+    return searchNode(root, key);
+  };
+
+  this.remove = function (key) {
+    root = removeNode(root, key);
+  };
+}
